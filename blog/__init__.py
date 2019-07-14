97ff45bc1e33585e9b42d442dd0305cf48c441a5
Christopher Clement Andreas <clement.andreas97@gmail.com>
2019-07-14 18:06:34 +0700

    Add initial blog system

---
+++ blog/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'c5e4546635faf88f7d1cb9c4ae23999a'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from blog import routes
---
+++ blog/mock.py
@@ -0,0 +1,14 @@
+posts = [
+    {
+        'author': 'Clement',
+        'title': 'Post 1',
+        'content': 'Post Content 1',
+        'date_posted': 'June 27, 2019'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Post 2',
+        'content': 'Post Content 2',
+        'date_posted': 'June 27, 2019'
+    }
+]
