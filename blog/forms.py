fb196c74d39e0c81c2179b1262a5ee4895cedfd2
Christopher Clement Andreas <clement.andreas97@gmail.com>
2019-07-24 21:29:07 +0700

    add crud functionality for post

--- blog/forms.py
+++ blog/forms.py
@@ -2,7 +2,7 @@ from blog.models import User
 from flask_login import current_user
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 class RegistrationForm(FlaskForm):
@@ -46,3 +46,9 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('The email is taken. Please choose a different one')
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+
+    submit = SubmitField('Post')
